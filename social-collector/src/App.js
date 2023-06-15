dab69aca1f64ec76399f44604033506c988f0fc8
tskinner429 <tskinner429@gmail.com>
2023-06-15 17:50:57 -0400

    moved homepage code over

--- social-collector/src/App.js
+++ social-collector/src/App.js
@@ -27,5 +27,17 @@ function App() {
     </div>
   );
 }
+import React from 'react';
+import './App.css';
+import Homepage from './Homepage';
+import Navbar from './Navbar';
 
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Homepage />
+    </div>
+  );
+}
 export default App;
