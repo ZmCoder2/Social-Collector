890f436ab8e2096ca0616f3a7b599d0ced2161df
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2023-06-15 18:08:07 -0400

    icons

--- social-collector/src/components/Navbar/nav.js
+++ social-collector/src/components/Navbar/nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom'
 import './index.css'
-// import { faHome }
+import { faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 function Nav() {
   return (
