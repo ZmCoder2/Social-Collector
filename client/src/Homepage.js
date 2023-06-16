a71b92b8dc947461487fa8e729f87b7712f5bb5c
August Tornea <129027081+evothinke@users.noreply.github.com>
2023-06-15 23:27:22 -0400

    Add files via upload

---
+++ client/src/Homepage.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const items = [
+  {
+    title: "Super Mario Odyssey",
+    category: "Video Games",
+    image: "mario_odyssey.jpg"
+  },
+  {
+    title: "Michael Jordan Autographed Basketball",
+    category: "Sports Memorabilia",
+    image: "jordan_basketball.jpg"
+  },
+
+  {
+    title: "One Piece Manga",
+    category: "Manga",
+    image: "one_piece_manga.jpg"
+  },
+  {
+    title: "Naruto Uzumaki Figure",
+    category: "Anime Collectibles",
+    image: "naruto_figure.jpg"
+  }
+  
+];
+
+const Homepage = () => {
+  const onItemClick = (item) => {
+
+    console.log("Clicked item:", item);
+  };
+
+  return (
+    <div>
+      <h1>Welcome to Social Collector!'</h1>
+      <div id="item-container">
+        {items.map((item, index) => (
+          <div
+            key={index}
+            className="item-card"
+            onClick={() => onItemClick(item)}
+          >
+            <h3>{item.title}</h3>
+            <p>{item.category}</p>
+            <img src={require(`./images/${item.image}`).default} alt={item.title} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
