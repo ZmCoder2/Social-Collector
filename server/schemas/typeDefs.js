e3f7a9642b15a529aada48ca2bce71092f6bb9a9
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2023-06-16 13:15:36 -0400

    helloooo

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,41 +1,37 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-
 scalar Date
-
   type Users {
     _id: ID!
     name: String!
     email: String!
-    password: String!
+    password: String
   
-  }
-  type Categories {
-    
-    title: String
-    dateCreated: Date
-  }
-  type categories {
-
-    title: String
-    dateCreated: String
   }
 
   type Auth {
     token: ID!
-    user: Users
+    users: Users
   }
 
   type Query {
     users: [Users]!
-    categories: [Categories]!
+    
   }
   
+  type Categories {
+    _id: ID!
+    title: String
+    dateCreated: String
+  }
 
   type Mutation {
     addUser(name: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
+
+    
+    
   }
 `;
 
