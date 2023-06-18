965630c157db03692e708e9635164fee59a1d751
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2023-06-17 21:05:51 -0400

    categories and posts now in seperate cards on the homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,16 +4,23 @@ import CategoryMenu from "../components/CategoryMenu";
 import CategoryList from "../components/Categories/categoryCard";
 import PostList from "../components/Posts/postsCard";
 import AddCategory from "../components/Categories/categoryCard";
+import './home.css';
 
 const Home = () => {
   return (
-    <div className="container">
-      <CategoryMenu />
-      <CategoryList />
-      <PostList />
-      <AddCategory />
+    <div className="card-container">
+      <div className="card">
+        {/* <h2>Categories</h2> */}
+        <CategoryList />
+        {/* <AddCategory /> */}
+      </div>
+      <div className="card">
+        {/* <h2>Posts</h2> */}
+        <PostList />
+      </div>
     </div>
   );
 };
 
 export default Home;
+
