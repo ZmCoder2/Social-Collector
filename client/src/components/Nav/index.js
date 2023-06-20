52461ab749c606e5b0d384362f9d0e6d801a6657
alec <amarierose19@example.com>
2023-06-20 01:51:15 -0400

    worked on homepage and tweaked some functionailty

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,86 +1,91 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function Nav() {
+  useEffect(() => {
+    const navbarToggler = document.querySelector(".navbar-toggler");
+    const navbarCollapse = document.querySelector(".navbar-collapse");
+    
+    navbarToggler.addEventListener("click", () => {
+      navbarCollapse.classList.toggle("show");
+    });
+  }, []);
 
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
         <ul className="flex-row">
+          <li className="mx-1"></li>
           <li className="mx-1">
-            <Link to="/">
-              Home
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/profile">
-              Profile
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/addcategory">
-              Add Category
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/addpost">
-              Add Post
-            </Link>
-          </li>
-            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
             <a href="/" onClick={() => Auth.logout()}>
               Logout
             </a>
+          </li>
         </ul>
       );
     } else {
       return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/">
-              Home
-            </Link>
-          </li>
-          {/* <li className="mx-1"> || i personally dont think we should have this because the user has not logged in
-            <Link to="/profile">
-              Profile
-            </Link>
-          </li> */}
-          <li className="mx-1">
-            <Link to="/signup">
-              Signup
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/login">
-              Login
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/addcategory">
-              Add Category
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/addpost">
-              Add Post
-            </Link>
-          </li>
-        </ul>
+        <nav className="navbar navbar-expand-lg">
+          <div className="container-fluid">
+            <button
+              className="navbar-toggler bg-body-tertiary"
+              type="button"
+              aria-controls="navbarTogglerDemo01"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div
+              className="collapse navbar-collapse justify-content-end"
+              id="navbarTogglerDemo01"
+            >
+              <ul className="navbar-nav flex-row">
+                <li className="nav-item mx-1">
+                  <Link to="/" className="nav-link">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item mx-1">
+                  <Link to="/profile" className="nav-link">
+                    Profile
+                  </Link>
+                </li>
+                <li className="nav-item mx-1">
+                  <Link to="/signup" className="nav-link">
+                    Signup
+                  </Link>
+                </li>
+                <li className="nav-item mx-1">
+                  <Link to="/login" className="nav-link">
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item mx-1">
+                  <Link to="/addcategory" className="nav-link">
+                    Add Cat
+                  </Link>
+                </li>
+                <li className="nav-item mx-1">
+                  <Link to="/addpost" className="nav-link">
+                    Add post
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       );
     }
   }
 
   return (
     <header className="flex-row px-1">
-      <h1>
-      
-      </h1>
-
-      <nav>
-        {showNavigation()}
-      </nav>
+      <h1></h1>
+      <nav>{showNavigation()}</nav>
     </header>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import shoes from "../assets/shoes.jpeg";
+import anime from "../assets/anime.webp";
+import workout from "../assets/workout.webp";
 import CategoryMenu from "../components/CategoryMenu";
 import CategoryList from "../components/Categories/categoryCard";
 import PostList from "../components/Posts/postsCard";
@@ -7,18 +9,123 @@ import AddCategory from "../components/Categories/categoryCard";
 import './home.css';
 
 const Home = () => {
-  return (
-    <div className="card-container">
-      <div className="card">
-        {/* <h2>Categories</h2> */}
+  
+    return (
+      <div className="background"> 
+    <div className="card" id= "category">
+        { /*<h2>Categories</h2>*/ }
+        
         <CategoryList />
-        {/* <AddCategory /> */}
-      </div>
-      <div className="card">
-        {/* <h2>Posts</h2> */}
-        <PostList />
-      </div>
+        {/* <AddCategory /> */} 
     </div>
+    
+<div className="card-group">
+  <div className="card cards">
+    <img src= {shoes} className="card-img-top" alt="..." />
+    <div className="card-body">
+      <h5 className="card-title">Shoe Collects</h5>
+      <p className="card-text">
+        Click Here For Shoes
+      </p>
+      <a href="#" className="btn btn-primary">
+        Go somewhere
+      </a>
+      <p className="card-text">
+        <small className="text-body-secondary">Last updated 3 mins ago</small>
+      </p>
+    </div>
+  </div>
+  <div className="card cards">
+    <img src= {anime} className="card-img-top" alt="..." />
+    <div className="card-body">
+      <h5 className="card-title">Anime Social</h5>
+      <p className="card-text">
+        Click here for Anime
+      </p>
+      <a href="#" className="btn btn-primary">
+        Go somewhere
+      </a>
+      <p className="card-text">
+        <small className="text-body-secondary">Last updated 3 mins ago</small>
+      </p>
+    </div>
+  </div>
+  <div className="card cards">
+    <img src= {workout} className="card-img-top" alt="..." />
+    <div className="card-body">
+      <h5 className="card-title">Workout Social</h5>
+      <p className="card-text">
+        Click here for Workout
+      </p>
+      <a href="#" className="btn btn-primary">
+        Go somewhere
+      </a>
+      <p className="card-text">
+        <small className="text-body-secondary">Last updated 3 mins ago</small>
+      </p>
+    </div>
+  </div>
+</div>
+<div className="card-group">
+  <div className="card cards">
+    <img src="..." className="card-img-top" alt="..." />
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </p>
+      <a href="#" className="btn btn-primary">
+        Go somewhere
+      </a>
+      <p className="card-text">
+        <small className="text-body-secondary">Last updated 3 mins ago</small>
+      </p>
+    </div>
+  </div>
+  <div className="card cards">
+    <img src="..." className="card-img-top" alt="..." />
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">
+        This card has supporting text below as a natural lead-in to additional
+        content.
+      </p>
+      <a href="#" className="btn btn-primary">
+        Go somewhere
+      </a>
+      <p className="card-text">
+        <small className="text-body-secondary">Last updated 3 mins ago</small>
+      </p>
+    </div>
+  </div>
+  <div className="card cards">
+    <img src="..." className="card-img-top" alt="..." />
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </p>
+      <a href="#" className="btn btn-primary">
+        Go somewhere
+      </a>
+      <p className="card-text">
+        <small className="text-body-secondary">Last updated 3 mins ago</small>
+      </p>
+    </div>
+  </div>
+</div>
+<footer className="bg-dark text-white-50 py-5 mt-5" id="sticky-footer">
+  <div className="footer text-lighter text-center">
+    <p className="display-5 mb-3">SocialCollectors</p>
+    <small className="text-center">
+      © Copyright by Group 12. All Rights Reserved
+    </small>
+  </div>
+</footer>
+  </div>
   );
 };
 
