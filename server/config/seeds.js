5802b00ac7fa01e20920fe3c1afdb119e3491cff
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2023-06-20 02:24:21 -0400

    config

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,124 +1,153 @@
 const db = require('./connection.js');
-const { User, Posts, Categories} = require('../models');
+const { User, Posts, Categories } = require('../models');
 
 db.once('open', async () => {
-
-    const categories = await Categories.insertMany([
-        { title: 'Vinyls' },
-        { title: 'Anime' },
-        { title: 'Shoes' },
-        { title: 'Art' },
-        { title: 'Sports' }
-      ]);
-      console.log('categories seeded');
+  try {
+    const category = await Categories.insertMany([
+      { name: 'Vinyls', posts: [] },
+      { name: 'Anime', posts: [] },
+      { name: 'Shoes', posts: [] },
+      { name: 'Vintage Video Games', posts: [] },
+      { name: 'Sports', posts: [] }
+    ])
+    console.log('categories seeded');
 
     const posts = await Posts.insertMany([
-        {
-          title: 'Tin of Cookies',
-          description:
-            'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-          image: 'cookie-tin.jpg',
-          category: categories[0]._id,
-        },
-        {
-          title: 'Canned Coffee',
-          description:
-            'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-          image: 'canned-coffee.jpg',
-          category: categories[0]._id,
-        },
-        {
-          title: 'Toilet Paper',
-          category: categories[1]._id,
-          description:
-            'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-          image: 'toilet-paper.jpg',
-        },
-        {
-          title: 'Handmade Soap',
-          category: categories[1]._id,
-          description:
-            'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-          image: 'soap.jpg',
-        },
-        {
-          title: 'Set of Wooden Spoons',
-          category: categories[1]._id,
-          description:
-            'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
-          image: 'wooden-spoons.jpg',
-        },
-        {
-          title: 'Camera',
-          category: categories[2]._id,
-          description:
-            'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-          image: 'camera.jpg',
-        },
-        {
-          title: 'Tablet',
-          category: categories[2]._id,
-          description:
-            'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
-          image: 'tablet.jpg',
-        },
-        {
-          title: 'Tales at Bedtime',
-          category: categories[3]._id,
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
-          image: 'bedtime-book.jpg',
-        },
-        {
-          title: 'Spinning Top',
-          category: categories[4]._id,
-          description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
-          image: 'spinning-top.jpg',
-        },
-        {
-          title: 'Set of Plastic Horses',
-          category: categories[4]._id,
-          description:
-            'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
-          image: 'plastic-horses.jpg',
-        },
-        {
-          title: 'Teddy Bear',
-          category: categories[4]._id,
-          description:
-            'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
-          image: 'teddy-bear.jpg',
-        },
-        {
-          title: 'Alphabet Blocks',
-          category: categories[4]._id,
-          description:
-            'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
-          image: 'alphabet-blocks.jpg',
-        }
-      ]);
-      console.log('products seeded');
-
-  await User.deleteMany();
-
-  await User.create({
-    username: 'Pamela2',
-    email: 'pamela@testmail.com',
-    password: 'password12345',
-    posts: [
       {
-        posts: [posts[0]._id, posts[0]._id, posts[1]._id]
+        title: 'Michael Jordan Signed Jersey',
+        description: 'Jersey is from 1991.',
+        category: category[4]._id,
+        userPosts: [],
+        comments: [],
+        image: null
+      },
+      {
+        title: 'One Piece Figurines',
+        description: 'The whole Straw Hat Pirate crew.',
+        category: category[1]._id,
+        userPosts: [],
+        comments: [],
+        // image: null
+      },
+      {
+        title: 'Travis Scott Jordan 1 lows',
+        description: 'Brand new never worn',
+        category: category[2]._id,
+        userPosts: [],
+        comments: [],
+        // image: null
+      },
+      {
+        title: 'Patriots 2019 Super Bowl Ring',
+        description:
+          'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
+        category: category[4]._id,
+        userPosts: [],
+        comments: [],
+        // image: null
+      },
+      {
+        title: 'Nirvana Vinyl Collection',
+        description: 'The whole set of Nirvana vinyls.',
+        category: category[0]._id,
+        userPosts: [],
+        comments: [],
+        // image: null
+      },
+      {
+        title: 'Nintendo 64 Games',
+        description:
+          'Super Mario 64, The Legend of Zelda: Ocarina of Time, GoldenEye 007, Mario Kart 64, The Legend of Zelda: Majoras Mask, Perfect Dark, Super Smash Bros., Banjo-Kazooie, Donkey Kong 64, Star Fox 64, Pokémon Stadium, Paper Mario, Diddy Kong Racing, Conkers Bad Fur Day, Wave Race 64, Mario Party, F-Zero X, Yoshis Story, Resident Evil 2, Bomberman 64.',
+        category: category[3]._id,
+        userPosts: [],
+        comments: [],
+        image: null
+      },
+      {
+        title: 'Vintage Pokemon cards',
+        description:
+          'Charizard, Blastoise, Venusaur, Pikachu, Mewtwo, Zapdos, Articuno, Moltres, Raichu, Dragonite, Gyarados, Alakazam, Jolteon, Flareon, Vaporeon, Aerodactyl, Arcanine, Lapras, Scyther, Hitmonchan.',
+        category: category[1]._id,
+        userPosts: [],
+        comments: [],
+        // image: null
+      },
+      {
+        title: 'Yeezy Boosts Collection',
+        description:
+          'Yeezy Boost 350 Turtle Dove, Pirate Black, Moonrock, Oxford Tan, Beluga, Zebra, Bred, Cream White, Sesame, Butter, Static Yeezy Boost 350 V2 Black/Red, Blue Tint, Semi Frozen Yellow, Clay, Hyperspace, True Form, Citrin, Antlia, Lundmark.',
+        category: category[2]._id,
+        userPosts: [],
+        comments: [],
+        // image: null
+      },
+      {
+        title: 'The Doors Vinyl Collection',
+        description:
+          'The Doors, Strange Days, Waiting for the Sun, The Soft Parade, Morrison Hotel, Absolutely Live, L.A. Woman, Other Voices, Full Circle, An American Prayer.',
+        category: category[0]._id,
+        userPosts: [],
+        comments: [],
+        // image: null
+      },
+      {
+        title: 'Halo Collection',
+        description: 'All of the Halo games and master chief xbox 360.',
+        category: category[3]._id,
+        userPosts: [],
+        comments: [],
+        // image: 'plastic-horses.jpg'
+      },
+      {
+        title: '2019 LSU Home Game Tickets',
+        description: 'All of LSUs Football game tickets from 2019.',
+        category: category[4]._id,
+        userPosts: [],
+        comments: [],
+        image: 'teddy-bear.jpg'
+      },
+      {
+        title: 'Jordan Shoe Collection',
+        description:
+          'Jordan 1, Jordan 3, Jordan 4, Jordan 5, Jordan 6, Jordan 11, Jordan 12, Jordan 13, Jordan 14, Jordan 23, Jordan 33, Jordan 34, Jordan 35, Jordan 36, Jordan 1 Low, Jordan 4 Retro, Jordan 6 Rings, Jordan Spizike, Jordan Son of Mars.',
+        category: category[2]._id,
+        usersPosts: [],
+        comments: [],
+        // image: null
       }
-    ]
-  });
+    ]);
+    console.log('posts seeded');
 
-  await User.create({
-    username: 'Elijah1',
-    email: 'eholt@testmail.com',
-    password: 'password12345'
-  });
+    await User.deleteMany();
 
-  console.log('users seeded');
+    await User.create({
+      username: 'Pamela5',
+      email: 'pamela@testmail.com',
+      password: 'rootp1234',
+      posts: [
+        {
+          posts: [posts[0]._id, posts[4]._id, posts[5]._id]
+        }
+      ]
+    });
 
-  process.exit();
+    await User.create({
+      username: 'Elijah2',
+      email: 'eholt@testmail.com',
+      password: 'roote1234',
+      posts: [
+        {
+          posts: [posts[1]._id, posts[2]._id, posts[3]._id]
+        }
+      ]
+    });
+
+    console.log('users seeded');
+
+    process.exit();
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 });
+
