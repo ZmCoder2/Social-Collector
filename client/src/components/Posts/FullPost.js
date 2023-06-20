b1138608844467b117a33bcef6dee1518325f226
August Tornea <august.tornea@gmail.com>
2023-06-20 02:39:20 -0400

    fullPost

---
+++ client/src/components/Posts/FullPost.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+import { Link, useParams } from 'react-router-dom';
+
+const GET_POSTS = gql`
+  query {
+    posts {
+      _id
+      title
+      description
+    }
+  }
+`;
+
+const PostDetails = () => {
+  const { postId } = useParams();
+  const { loading, error, data } = useQuery(GET_POSTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error fetching posts: {error.message}</p>;
+
+  const post = data.posts.find((post) => post._id === String(postId));
+
+  if (!post) return <p>Post not found.</p>;
+
+  return (
+    <div>
+      <h3>{post.title}</h3>
+      <p>{post.description}</p>
+      <Link to="/posts">Back to Posts</Link>
+    </div>
+  );
+};
+
+export default PostDetails;
