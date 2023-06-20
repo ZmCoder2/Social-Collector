4131f2ae355e90e47890bd7d983f7cd041ce2207
August Tornea <august.tornea@gmail.com>
2023-06-20 02:40:47 -0400

    nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,86 +1,157 @@
 import React from "react";
 import Auth from "../../utils/auth";
-import { Link } from "react-router-dom";
 
 function Nav() {
-
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/">
-              Home
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/profile">
-              Profile
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/addcategory">
-              Add Category
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/addpost">
-              Add Post
-            </Link>
-          </li>
-            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-            <a href="/" onClick={() => Auth.logout()}>
-              Logout
+        <nav className="bg-teal-500 shadow-md mb-4">
+          <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
+            <a href="/" className="flex items-center">
+              <img src="" className="h-8 mr-3" alt="" />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap text-white dark:text-white">
+                Socialitae
+              </span>
             </a>
-        </ul>
+            <button
+              data-collapse-toggle="navbar-default"
+              type="button"
+              className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-default"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+            <div
+              className="hidden w-full md:block md:w-auto"
+              id="navbar-default"
+            >
+              <ul className="flex flex-col p-4 md:p-0 mt-4 border-t border-gray-300 md:flex-row md:mt-0 md:border-0">
+                <li className="mr-4">
+                  <a
+                    href="/"
+                    className="text-white hover:text-teal-200"
+                    aria-current="page"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li className="mr-4">
+                  <a
+                    href="/Posts"
+                    className="text-white hover:text-teal-200"
+                  >
+                    Posts
+                  </a>
+                </li>
+                <li className="mr-4">
+                  <a
+                    href="/Profile"
+                    className="text-white hover:text-teal-200"
+                  >
+                    Profile
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    onClick={() => Auth.logout()}
+                    className="text-white hover:text-teal-200"
+                  >
+                    Logout
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       );
     } else {
       return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/">
-              Home
-            </Link>
-          </li>
-          {/* <li className="mx-1"> || i personally dont think we should have this because the user has not logged in
-            <Link to="/profile">
-              Profile
-            </Link>
-          </li> */}
-          <li className="mx-1">
-            <Link to="/signup">
-              Signup
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/login">
-              Login
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/addcategory">
-              Add Category
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/addpost">
-              Add Post
-            </Link>
-          </li>
-        </ul>
+        <nav className="bg-teal-500 shadow-md mb-4">
+          <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
+            <a href="/" className="flex items-center">
+              <img src="" className="h-8 mr-3" alt="" />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap text-white dark:text-white">
+                Socialitae
+              </span>
+            </a>
+            <button
+              data-collapse-toggle="navbar-default"
+              type="button"
+              className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-default"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+            <div
+              className="hidden w-full md:block md:w-auto"
+              id="navbar-default"
+            >
+              <ul className="flex flex-col p-4 md:p-0 mt-4 border-t border-gray-300 md:flex-row md:mt-0 md:border-0">
+                <li className="mr-4">
+                  <a
+                    href="/"
+                    className="text-white hover:text-teal-200"
+                    aria-current="page"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li className="mr-4">
+                  <a
+                    href="/Posts"
+                    className="text-white hover:text-teal-200"
+                  >
+                    Posts
+                  </a>
+                </li>
+                <li className="mr-4">
+                  <a
+                    href="/login"
+                    className="text-white hover:text-teal-200"
+                  >
+                    Login
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       );
     }
   }
 
   return (
-    <header className="flex-row px-1">
-      <h1>
-      
-      </h1>
-
-      <nav>
-        {showNavigation()}
-      </nav>
+    <header className="flex-row px-0">
+      <nav>{showNavigation()}</nav>
     </header>
   );
 }
