c366c0a07b17fa6909e43b8f5ca3a162b2c9e6b1
August Tornea <august.tornea@gmail.com>
2023-06-20 02:38:55 -0400

    postCard

--- client/src/components/Posts/postsCard.js
+++ client/src/components/Posts/postsCard.js
@@ -18,12 +18,20 @@ const PostList = () => {
 
   return (
     <div>
-      <h2>Posts</h2>
-      {data.posts.map((post) => (
-        <div key={post._id}>
-          <h3>{post.title}</h3>
-        </div>
-      ))}
+      <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Latest Posts</span>
+      </h1>
+      <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400"></p>
+      
+      <ul className="list-disc pl-6">
+        {data.posts.map((post) => (
+          <li key={post._id}>
+            <a href={`/posts/${post._id}`}>
+              <h3>{post.title}</h3>
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
