705591a3bd6bd325a253fd21219ea29cb9d6732d
August Tornea <august.tornea@gmail.com>
2023-06-20 02:35:19 -0400

    login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -30,38 +30,69 @@ function Login(props) {
   };
 
   return (
-    <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
-
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
+    <div className="container my-1 mx-auto">
+      <form className="w-full max-w-sm mx-auto" onSubmit={handleFormSubmit}>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-email">
+              Email address
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              id="inline-email"
+              type="email"
+              name="email"
+              placeholder="youremail@test.com"
+              value={formState.email}
+              onChange={handleChange}
+            />
+          </div>
         </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-password">
+              Password
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              id="inline-password"
+              type="password"
+              name="password"
+              placeholder="******************"
+              value={formState.password}
+              onChange={handleChange}
+            />
+          </div>
         </div>
-        {error ? (
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3"></div>
+          <label className="md:w-2/3 block text-gray-500 font-bold">
+            <input className="mr-2 leading-tight" type="checkbox" />
+           
+          </label>
+        </div>
+        {error && (
           <div>
             <p className="error-text">The provided credentials are incorrect</p>
           </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
+        )}
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3"></div>
+          <div className="md:w-2/3 flex justify-between">
+            <button
+              className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              type="submit"
+            >
+              Sign In
+            </button>
+            <Link to="/signup" className="text-purple-500 hover:text-purple-400 font-bold">
+              Register
+            </Link>
+          </div>
         </div>
       </form>
     </div>
