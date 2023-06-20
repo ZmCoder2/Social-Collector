3e9c3b89ff81d6f7aafbdabc5e29e4c974141dde
August Tornea <august.tornea@gmail.com>
2023-06-20 02:33:59 -0400

    home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,26 +1,21 @@
 import React from "react";
-
-import CategoryMenu from "../components/CategoryMenu";
 import CategoryList from "../components/Categories/categoryCard";
 import PostList from "../components/Posts/postsCard";
-import AddCategory from "../components/Categories/categoryCard";
-import './home.css';
+import "../index.css";
 
 const Home = () => {
   return (
-    <div className="card-container">
-      <div className="card">
-        {/* <h2>Categories</h2> */}
-        <CategoryList />
-        {/* <AddCategory /> */}
-      </div>
-      <div className="card">
-        {/* <h2>Posts</h2> */}
+    <div className="container mx-auto flex flex-wrap">
+      <div className="card shadow-lg rounded-lg p-4 w-full md:w-1/2 order-2 md:order-1">
+        
         <PostList />
+      </div>
+      <div className="card shadow-lg rounded-lg p-4 w-full md:w-1/2 order-1 md:order-2 mt-4 md:mt-0">
+      
+        <CategoryList />
       </div>
     </div>
   );
 };
 
 export default Home;
-
